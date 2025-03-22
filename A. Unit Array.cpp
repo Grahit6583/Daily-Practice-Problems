059cb7d3239291a64c0b28743a61418f45d63966
Grahit6583 <grahitgoyal4545@gmail.com>
2025-03-22 23:51:30 +0530

    Codeforces 800

---
+++ A. Unit Array.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+
+        int pos = 0;
+        int neg = 0;
+        for(int i=0; i<n; i++){
+            int x;
+            cin>>x;
+
+            if(x > 0){
+                pos++;
+            }
+            else{
+                neg++;
+            }
+        }
+
+        if()
+    }
+
+    return 0;
+}
