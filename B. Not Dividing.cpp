4f51f87fd303b0ffed89c8e13bffe23cbfa7ef66
Grahit6583 <grahitgoyal4545@gmail.com>
2025-04-09 17:50:52 +0530

    Codeforces 900

--- B. Not Dividing.cpp
+++ B. Not Dividing.cpp
@@ -12,30 +12,30 @@ int main(){
     while(t--){
         int n;
         cin>>n;
-
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
         
         vector<int> arr(n);
         for(int i=0; i<n; i++){
             cin>>arr[i];
         }
-
-        int ans = arr[n-1]-arr[0];
-
-        for(int i=1; i<n; i++){
-            ans = max(ans,arr[i]-arr[0]);
+        for(int i=0; i<n; i++){
+            if(arr[i] == 1){
+                arr[i]++;
+            }
         }
-
         for(int i=0; i<n-1; i++){
-            ans = max(ans, arr[n-1]-arr[i]);
+            if(arr[i] == 1){
+                arr[i]++;
+            }
+
+            if(arr[i+1] % arr[i] == 0){
+                arr[i+1] += 1;
+            }
         }
 
-        for(int i=0; i<n-1; i++){
-            ans = max(ans,arr[i]-arr[i+1]);
+        for(int i=0; i<n; i++){
+            cout<<arr[i]<<" ";
         }
-
-        cout<<ans<<"\n";
+        cout<<"\n";
     }
 
     return 0;
